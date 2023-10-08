dd8366a5d4a35c1a406659335d1c90409565bede
chrxtian <los9dragones@gmail.com>
2023-10-08 09:43:53 -0500

    Add project files.

---
+++ Dotnet6-DesignPattern/FactoryMethod/Discount/DiscountFactory.cs
@@ -0,0 +1,110 @@
+﻿namespace Dotnet6_DesignPattern.FactoryMethod.Discount
+{
+    /// <summary>
+    /// Product
+    /// </summary>
+    public abstract class DiscountService
+    {
+        public abstract int DiscountPercentage { get; }
+
+        public override string ToString() => GetType().Name;
+    }
+
+    /// <summary>
+    /// ConcreteProduct 1
+    /// </summary>
+    public class CountryDiscountService : DiscountService
+    {
+        private readonly string _countryCode;
+
+        public CountryDiscountService(string countryIdentifier)
+        {
+            _countryCode = countryIdentifier;
+        }
+
+        public override int DiscountPercentage
+        {
+            get
+            {
+                switch (_countryCode)
+                {
+                    // This countries gets a better discount XD
+                    case "PH":
+                    case "PE":
+                        return 20;
+                    default:
+                        return 10;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// /// ConcreteProduct 1
+    /// </summary>
+    public class CodeDiscountService : DiscountService
+    {
+        private readonly Guid _code;
+        public CodeDiscountService(Guid code)
+        {
+            _code = code;
+        }
+
+        public override int DiscountPercentage
+        {
+            // This is an example, but here we can add additional validations
+            // for check that the code is only used once.
+            get => 15;
+        }
+    }
+
+    //======================================================================//
+
+    /// <summary>
+    /// Creator
+    /// </summary>
+    public abstract class DiscountFactory
+    {
+        public abstract DiscountService CreateDiscountService();
+    }
+
+    /// <summary>
+    /// ConcreteCreator 1
+    /// </summary>
+    public class CountryDiscountFactory : DiscountFactory
+    {
+        private readonly string _countryIdentifier;
+        public CountryDiscountFactory(string countryIdentifier)
+        {
+            _countryIdentifier = countryIdentifier;
+        }
+
+        public override DiscountService CreateDiscountService()
+        {
+            return new CountryDiscountService(_countryIdentifier);
+        }
+    }
+
+    /// <summary>
+    /// ConcreteCreator 2
+    /// </summary>
+    public class CodeDiscountFactory : DiscountFactory
+    {
+        private readonly Guid _code;
+        public CodeDiscountFactory(Guid code)
+        {
+            _code = code;
+        }
+
+        public override DiscountService CreateDiscountService()
+        {
+            return new CodeDiscountService(_code);
+        }
+    }
+
+
+
+
+
+
+}
---
+++ Dotnet6-DesignPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using Dotnet6_DesignPattern.FactoryMethod.Discount;
+
+namespace Dotnet6_DesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Factory Method");
+            Console.WriteLine("--------------");
+
+            var factories = new List<DiscountFactory>
+            {
+                new CodeDiscountFactory(Guid.NewGuid()),
+                new CountryDiscountFactory("PE")
+            };
+
+            foreach (var factory in factories)
+            {
+                var discountService = factory.CreateDiscountService();
+
+                Console.WriteLine($"{discountService.DiscountPercentage}% of discount from {discountService}");
+            }
+        }
+    }
+}
